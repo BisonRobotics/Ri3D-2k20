bc3e7bd70cf64f20b21703de0633bf45e44f83cd
Andrew Vetter <andrewVetter@ScientificPineapple.com>
2020-01-06 15:51:56 -0600

    update motor ids. Not all the way done. Changes committed:
            modified:   4818/src/main/java/org/wfrobotics/robot/config/RobotConfig.java

--- 4818/src/main/java/org/wfrobotics/robot/config/RobotConfig.java
+++ 4818/src/main/java/org/wfrobotics/robot/config/RobotConfig.java
@@ -17,10 +17,10 @@ public class RobotConfig
     }
     public class DriveConfig
     {
-        public int rightMaster =  0;
-        public int rightSlave = 1;
-        public int leftMaster = 2;
-        public int leftSlave = 3;
+        public int rightMaster =  12;
+        public int rightSlave = 17;
+        public int leftMaster = 13;
+        public int leftSlave = 14;
         public double maxSpeed = 1.0; //exactly what it sounds like
         // Set this to true if bot only spins when you try to go forward or back
         public boolean rightSideInverted = false;
@@ -29,26 +29,26 @@ public class RobotConfig
     }
     public class ShooterConfig
     {
-        public int belt = 4;
+        public int belt = 22;
         public int flywheel = 7;
 
     }
     public class ClimbConfig
     {
-        public int climbMaster = 5;
-        public int climbSlave = 7;
+        public int climbMaster = 10;
+        public int climbSlave = 16;
 
-        public int upCdayzi = 9;
+        public int upCdayzi = 21;
 
     }
     public class WofFConfig
     {
-        public int spinner = 5;
+        public int spinner = 23;
     }
     public class IntakeConfig
     {
-        public int intake = 6;
-        public int loader = 9;
+        public int intake = 8;
+        public int loader = 24;
     }
 
     private static RobotConfig instance = null;
